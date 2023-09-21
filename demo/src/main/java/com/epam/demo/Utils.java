2d4a13bbb8ddf695fac804072ec08d04050d330a
DmitryAnisimovJava <130083398+DmitryAnisimovJava@users.noreply.github.com>
2023-09-21 17:29:33 +0300

    Done.

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,9 +1,11 @@
 package com.epam.demo;
 
+import java.util.Iterator;
 import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-       //magic happens here
+     boolean result = args.stream().allMatch(stringList -> Double.parseDouble(stringList) > 0);
+     return result;
     }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,7 +1,14 @@
 package com.epam.utils;
 
+import java.util.function.Function;
+
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        //code!
+    	Function<String, Boolean> result = stringFrom -> {
+    		boolean res = Double.parseDouble(stringFrom) > 0 ? 
+    				true : false;
+    		return res;
+    	};
+    	return result.apply(str);
     }
 }
